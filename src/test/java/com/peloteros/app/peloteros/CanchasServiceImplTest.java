655b810d1bb7855b99810be2142577c9fe26a887
Harvey Guerrero Laban <109876821+Guerrero7557@users.noreply.github.com>
2024-12-05 00:52:11 -0500

    Correccion del erro e implementación del test

---
+++ src/test/java/com/peloteros/app/peloteros/CanchasServiceImplTest.java
@@ -0,0 +1,47 @@
+package com.peloteros.app.peloteros;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.peloteros.app.peloteros.entity.Canchas;
+import com.peloteros.app.peloteros.repository.CanchasRepository;
+import com.peloteros.app.peloteros.service.CanchasServiceImpl;
+
+@SpringBootTest
+public class CanchasServiceImplTest {
+
+    @Autowired
+    private CanchasServiceImpl canchasService;
+
+    @Autowired
+    private CanchasRepository canchasRepository;
+
+    @BeforeEach
+    public void setUp() {
+        canchasRepository.deleteAll();
+    }
+
+    @Test
+    public void testInsert() {
+        Canchas cancha = new Canchas();
+        cancha.setNumeroCancha(5);
+        cancha.setDescripcion("Cancha de fútbol");
+        cancha.setEstado("Disponible");
+        cancha.setImagen("imagen.png");
+        cancha.setPrecioPorHora(100.0); 
+
+        canchasService.insert(cancha);
+
+        Canchas foundCancha = canchasRepository.findById(cancha.getCanchaID()).orElse(null);
+        assertNotNull(foundCancha);
+        assertEquals(5, foundCancha.getNumeroCancha());
+        assertEquals("Cancha de fútbol", foundCancha.getDescripcion());
+        assertEquals("Disponible", foundCancha.getEstado());
+        assertEquals("imagen.png", foundCancha.getImagen());
+        assertEquals(100.0, foundCancha.getPrecioPorHora());
+    }
+}
